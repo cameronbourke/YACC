08a424e781988cd52f19f3db9fe37637d14616c8
cameronbourke <cameron2295@gmail.com>
2019-05-21 20:47:09 +1000

    Add unit tests for Recognizer

--- compiler/Recognizer/__tests__/Recognizer.test.js
+++ compiler/Recognizer/__tests__/Recognizer.test.js
@@ -1,212 +1,306 @@
 
-const Scanner = require('../sol/Scanner');
-const Token = require('../Token');
+const Scanner = require('../../Scanner/sol/Scanner');
+const Recognizer = require('../sol/Recognizer');
 
-describe('Scanner', () => {
+function testRecognizer (program) {
+	return () => new Recognizer(Scanner(program));
+}
 
-	it("should handle the empty program", () => {
-		const program = "";
-		const scanner = Scanner(program);
-		expect(() => {
-			const itr = scanner.next();
-			expect(itr.done).toEqual(false);
-			expect(itr.value).toEqual(new Token(Token.EOF, "$"));
-		}).not.toThrow();
+describe('Recognizer', () => {
+
+	describe("INTLITERAL", () => {
+		describe("valid programs", () => {
+			it("recognises int literal: 1", () => {
+				const program = "1";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+		});
+
+		describe("invalid programs", () => {
+			it("rejects space seperated list: 1 3", () => {
+				const program = "1 3";
+				expect(testRecognizer(program)).toThrow();
+			});
+		});
 	});
 
-	it('should handle whitespace', () => {
-		const programs = [" ", "  ", "	", "		", `	`];
-		programs.forEach(program => {
-			const scanner = Scanner(program);
-			expect(() => {
-				const itr = scanner.next();
-				expect(itr.done).toEqual(false);
-				expect(itr.value).toEqual(new Token(Token.EOF, "$"));
-			}).not.toThrow();
+	describe("Parens ( )", () => {
+		describe("valid programs", () => {
+			it("recognises grouped int literal: (1)", () => {
+				const program = "(1)";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises nested grouped int literal: ((1))", () => {
+				const program = "((1))";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+		});
+
+		describe("invalid programs", () => {
+			it("rejects empty group: ()", () => {
+				const program = "()";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects unclosed paren: (23", () => {
+				const program = "(23";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects trailing paren: 54)", () => {
+				const program = "54)";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects unbalanced parens: ((56)", () => {
+				const program = "((56)";
+				expect(testRecognizer(program)).toThrow();
+			});
 		});
 	});
 
-	describe("single character tokens", () => {
+	describe("Addition +", () => {
+		describe("valid programs", () => {
+			it("recognises single binary expr: 1 + 2", () => {
+				const program = "1 + 2";
+				expect(testRecognizer(program)).not.toThrow();
+			});
 
-		it("should recognise +", () => {
-			const program = "+";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.PLUS, "+")
+			it("recognises seq of additions: 1 + 2 + 3", () => {
+				const program = "1 + 2 + 3";
+				expect(testRecognizer(program)).not.toThrow();
 			});
 		});
 
-		it("should recognise -", () => {
-			const program = "-";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.MINUS, "-")
+		describe("invalid programs", () => {
+			it("rejects plus without operands: +", () => {
+				const program = "+";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-		it("should recognise /", () => {
-			const program = "/";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.DIV, "/")
+			it("does not support postive unary prefix operator: +1", () => {
+				const program = "+1";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-		it("should recognise *", () => {
-			const program = "*";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.MULT, "*")
+			it("does not support positive unary postfix operator: 1+", () => {
+				const program = "1+";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-		it("should recognise (", () => {
-			const program = "(";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.LPAREN, "(")
+			it("does not support ++ unary prefix operator: ++1", () => {
+				const program = "++1";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-		it("should recognise )", () => {
-			const program = ")";
-			const scanner = Scanner(program);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.RPAREN, ")")
+			it("does not support ++ unary postfix operator: 1++", () => {
+				const program = "1++";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-	});
+			describe("when there is one operand", () => {
+				it("rejects: 1 +", () => {
+					const program = "1 +";
+					expect(testRecognizer(program)).toThrow();
+				});
 
-	describe("int literals", () => {
-		it("should recognise an integer", () => {
-			const literals = ["2", "38", "694"];
-			literals.forEach(literal => {
-				const scanner = Scanner(literal);
-				expect(scanner.next()).toEqual({
-					done: false,
-					value: new Token(Token.INTLITERAL, literal)
+				it("rejects: + 1", () => {
+					const program = "+ 1";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: + 1 +", () => {
+					const program = "+ 1 +";
+					expect(testRecognizer(program)).toThrow();
 				});
 			});
 		});
-
-		it("should recognise integers", () => {
-			const program = "2 38 694";
-			const scanner = Scanner(program);
-
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.INTLITERAL, "2"),
-			});
-
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.INTLITERAL, "38"),
-			});
-
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.INTLITERAL, "694"),
-			});
-		});
-
-		it("should recognise negatively signed integers as two tokens", () => {
-			const literal = "-14";
-			const scanner = Scanner(literal);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.MINUS, "-"),
-			});
-
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.INTLITERAL, "14"),
-			});
-		});
-
-		it("should recognise positively signed integers as two tokens", () => {
-			const literal = "+14";
-			const scanner = Scanner(literal);
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.PLUS, "+"),
-			});
-
-			expect(scanner.next()).toEqual({
-				done: false,
-				value: new Token(Token.INTLITERAL, "14"),
-			});
-		});
-
 	});
 
+	describe("Subtraction -", () => {
+		describe("valid programs", () => {
+			it("recognises single binary expr: 5 - 6", () => {
+				const program = "5 - 6";
+				expect(testRecognizer(program)).not.toThrow();
+			});
 
-	describe("programs", () => {
-
-		it("test program 1", () => {
-			const program = "2 + 2";
-			const scanner = Scanner(program);
-			const tokens = [
-				new Token(Token.INTLITERAL, "2"),
-				new Token(Token.PLUS, "+"),
-				new Token(Token.INTLITERAL, "2"),
-				new Token(Token.EOF, "$"),
-			];
-			tokens.forEach((token, idx) => {
-				expect(scanner.next().value)
-				.toEqual(tokens[idx]);
+			it("recognises seq of additions: 5 - 6 - 7", () => {
+				const program = "5 - 6 - 7";
+				expect(testRecognizer(program)).not.toThrow();
 			});
 		});
 
-		it("test program 2", () => {
-			const program = "3 / 4 + 5 * 6 (7 * 8)";
-			const scanner = Scanner(program);
-			const tokens = [
-				new Token(Token.INTLITERAL, "3"),
-				new Token(Token.DIV, "/"),
-				new Token(Token.INTLITERAL, "4"),
-				new Token(Token.PLUS, "+"),
-				new Token(Token.INTLITERAL, "5"),
-				new Token(Token.MULT, "*"),
-				new Token(Token.INTLITERAL, "6"),
-				new Token(Token.LPAREN, "("),
-				new Token(Token.INTLITERAL, "7"),
-				new Token(Token.MULT, "*"),
-				new Token(Token.INTLITERAL, "8"),
-				new Token(Token.RPAREN, ")"),
-				new Token(Token.EOF, "$"),
-			];
-			tokens.forEach((token, idx) => {
-				expect(scanner.next().value)
-				.toEqual(tokens[idx]);
+		describe("invalid programs", () => {
+			it("rejects minus without operands: -", () => {
+				const program = "-";
+				expect(testRecognizer(program)).toThrow();
 			});
-		});
 
-		it("test program 3", () => {
-			const program = "((8+32))";
-			const scanner = Scanner(program);
-			const tokens = [
-				new Token(Token.LPAREN, "("),
-				new Token(Token.LPAREN, "("),
-				new Token(Token.INTLITERAL, "8"),
-				new Token(Token.PLUS, "+"),
-				new Token(Token.INTLITERAL, "32"),
-				new Token(Token.RPAREN, ")"),
-				new Token(Token.RPAREN, ")"),
-				new Token(Token.EOF, "$"),
-			];
-			tokens.forEach((token, idx) => {
-				expect(scanner.next().value)
-				.toEqual(tokens[idx]);
+			it("does not support postive unary prefix operator: -5", () => {
+				const program = "-5";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("does not support positive unary postfix operator: 5-", () => {
+				const program = "5-";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("does not support -- unary prefix operator: --5", () => {
+				const program = "--5";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("does not support -- unary postfix operator: 5--", () => {
+				const program = "5--";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			describe("when there is one operand", () => {
+				it("rejects: 5 -", () => {
+					const program = "5 -";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: - 5", () => {
+					const program = "- 5";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: - 5 -", () => {
+					const program = "- 5 -";
+					expect(testRecognizer(program)).toThrow();
+				});
 			});
 		});
-	
 	});
 
+	describe("Multiplication *", () => {
+		describe("valid programs", () => {
+			it("recognises single binary expr: 12 * 13", () => {
+				const program = "12 * 13";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises seq of multiplications: 12 * 13 * 14", () => {
+				const program = "12 * 13 * 14";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+		});
+
+		describe("invalid programs", () => {
+			it("rejects multiplication without operands: *", () => {
+				const program = "*";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			describe("when there is one operand", () => {
+				it("rejects: 12 *", () => {
+					const program = "12 *";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: * 12", () => {
+					const program = "* 12";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: * 12 *", () => {
+					const program = "* 12 *";
+					expect(testRecognizer(program)).toThrow();
+				});
+			});
+		});
+	});
+
+	describe("Division /", () => {
+		describe("valid programs", () => {
+			it("recognises single binary expr: 25 / 29", () => {
+				const program = "25 / 29";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises seq of divisions: 25 / 29 / 35", () => {
+				const program = "25 / 29 / 35";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+		});
+
+		describe("invalid programs", () => {
+			it("rejects division without operands: /", () => {
+				const program = "/";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			describe("when there is one operand", () => {
+				it("rejects: 25 /", () => {
+					const program = "25 /";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: / 25", () => {
+					const program = "/ 25";
+					expect(testRecognizer(program)).toThrow();
+				});
+
+				it("rejects: / 25 /", () => {
+					const program = "/ 25 /";
+					expect(testRecognizer(program)).toThrow();
+				});
+			});
+		});
+	});
+
+	describe("Test programs", () => {
+		describe("valid programs", () => {
+			it("recognises: (1 + 2 * 3)", () => {
+				const program = "(1 + 2 * 3)";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises: (1 + (2 * 3))", () => {
+				const program = "(1 + (2 * 3))";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises: ((1) + (2 * 3))", () => {
+				const program = "((1) + (2 * 3))";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises: (1 / 2) * 385 - 4 + 22", () => {
+				const program = "(1 / 2) * 385 - 4 + 22";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+
+			it("recognises: 1 * 2 + 3 / 4 - 5 + (582 * (220) + (1 / 49) - 53)", () => {
+				const program = "1 * 2 + 3 / 4 - 5 + (582 * (220) + (1 / 49) - 53)";
+				expect(testRecognizer(program)).not.toThrow();
+			});
+		});
+
+		describe("invalid programs", () => {
+			it("rejects: + 1 2 * 33", () => {
+				const program = "+ 1 2 * 33";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects: 13 --- 39", () => {
+				const program = "13 --- 39";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects: 5 (( * 17 / --- (", () => {
+				const program = "5 (( * 17 / --- (";
+				expect(testRecognizer(program)).toThrow();
+			});
+
+			it("rejects: 438 * -234", () => {
+				const program = "438 * -234";
+				expect(testRecognizer(program)).toThrow();
+			});
+		});
+	});
 });
